f94d8f8e067f132a1a8f74b551d313ab0575a729
tiaan-jonker <tiaanj06@gmail.com>
2022-06-27 11:33:12 +1200

    Event bus service

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const server = express()
+server.use(bodyParser.json())
+
+const apiOne = 'https://localhost:4000/events'
+const apiTwo = 'https://localhost:4001/events'
+const apiThree = 'https://localhost:4002/events'
+
+server.post('/events', (req, res) => {
+  const event = req.body
+
+  axios.post(apiOne, event).catch((error) => console.log(error.message))
+  axios.post(apiTwo, event).catch((error) => console.log(error.message))
+  axios.post(apiThree, event).catch((error) => console.log(error.message))
+
+  res.send({ status: 'OK' })
+})
+
+const PORT = 4005
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
