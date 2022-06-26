b19506c90768356f3b127e31ec22926905ec9b2e
tiaan-jonker <tiaanj06@gmail.com>
2022-06-27 09:35:40 +1200

    Posts service implemented

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A very basic app to learn the fundamentals of microservices
 
+## Posts Service
+
 | Path   | Method | Body |           Function |
 | :----- | :----: | :--: | -----------------: |
 | /posts |  POST  |      |  Create a new post |
---
+++ posts/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+const server = express()
+server.use(bodyParser.json())
+
+const posts = {}
+
+server.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+server.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+
+  posts[id] = {
+    id,
+    title,
+  }
+
+  res.status(201).send(posts[id])
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
